180904f92253d1f354c9b2e4343c1efb4d6e5539
flrl <flrl@users.noreply.github.com>
2010-01-16 08:38:20 +0000

    Started ast.[ch]

---
+++ ast.h
@@ -0,0 +1,64 @@
+/*
+ *  ast.h
+ *  Untitled
+ *
+ *  Created by Ellie on 16/01/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _AST_H
+
+#include <list>
+
+#include "variant.h"
+
+namespace Basic {
+    class ASTNode;
+    class Expression;
+    class Block;
+    class Statement;
+}
+
+using namespace Basic;
+
+class Basic::ASTNode {
+public:
+    virtual void execute(void) =0;
+    int getLine(void) { return line; }
+    int getColumn(void) { return column; }
+    void setPosition(int line, int column) { this->line = line; this->column = column; }
+protected:
+    int line;
+    int column;
+};
+
+class Basic::Expression : public ASTNode {
+public:
+    Expression();
+    Variant getResult();
+protected:
+    
+};
+
+class Basic::Block : public ASTNode {
+public:
+    Block();
+    ~Block();
+    void appendStatement(const Statement &);
+protected:
+    std::list<Statement> statements;
+};
+
+class Basic::Statement : public ASTNode {
+public:
+    Statement(StatementType);
+    ~Statement();
+protected:
+    
+};
+
+
+
+
+#endif
