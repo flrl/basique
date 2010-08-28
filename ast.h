f445d947fbdd6dc5c0f3b5a2614d76412865b6df
flrl <flrl@users.noreply.github.com>
2010-08-28 15:06:44 +0000

    Started hooking up AST stuff

--- ast.h
+++ ast.h
@@ -11,17 +11,32 @@
 
 #include <list>
 
+#include "tokeniser.h"
 #include "variant.h"
 
 namespace Basic {
     class ASTNode;
-    class Identifier;
+
+    class ParamList;
+    
     class Expression;
-    class Block;
+    class FunctionCallExpression;
+    class ArrayIndexExpression;
+    class VariableExpression;
+    class LiteralExpression;
+    class UnaryExpression;
+    class MultiplicativeExpression;
+    class AdditiveExpression;
+    class ComparitiveExpression;
+    class AndExpression;
+    class OrExpression;
+    
     class Statement;
     class PrintStatement;
-    class SemicolonExpression;
-    class CommaExpression;
+
+    class Block;
+    
+    class Identifier;
     class ForStatement;
     class DoStatement;
 }
@@ -39,54 +54,164 @@ protected:
     int column;
 };
 
-class Basic::Identifier : public ASTNode {
+class Basic::ParamList : public ASTNode {
 public:
-    Identifier();
-protected:
+    ParamList() { }
+    ~ParamList();
+    virtual void execute();
+    void appendExpression(Expression *e) { expressions.push_back(e); }
+private:
+    std::list<Expression *> expressions;
 };
+
 
 class Basic::Expression : public ASTNode {
 public:
-    Expression();
-    Variant getResult() { return result; }
+    Expression() { }
+    Variant getResult() { return value; }
 protected:
-    Variant result;
-    
+    Variant value;
 };
+
+class Basic::FunctionCallExpression : public Expression {
+public:
+    FunctionCallExpression(const char *id, ParamList *p) : params(p) {
+        strncpy(this->identifier, id, MAX_IDENTIFIER_LENGTH);        
+    }
+    ~FunctionCallExpression() { delete params; }
+    virtual void execute();
+private:
+    char identifier[MAX_IDENTIFIER_LENGTH + 1];
+    ParamList *params;
+};
+
+class Basic::ArrayIndexExpression : public Expression {
+public:
+    ArrayIndexExpression(const char *id, Expression *first, Expression *second) : first(first), second(second) {
+        strncpy(this->identifier, id, MAX_IDENTIFIER_LENGTH);
+    }
+    ~ArrayIndexExpression() { delete first; delete second; }
+    virtual void execute();
+private:
+    Expression *first;
+    Expression *second;
+    char identifier[MAX_IDENTIFIER_LENGTH + 1];
+};
+
+class Basic::VariableExpression : public Expression {
+public:
+    VariableExpression(const char *id) { strncpy(this->identifier, id, MAX_IDENTIFIER_LENGTH); }
+    virtual void execute();
+private:
+    char identifier[MAX_IDENTIFIER_LENGTH + 1];
+};
+
+class Basic::LiteralExpression : public Expression {
+public:
+    LiteralExpression(Variant &v) { this->value = v; }
+    virtual void execute();
+};
+
+class Basic::UnaryExpression : public Expression {
+public:
+    UnaryExpression(Token t, Expression *e) : op(t), term(e) { }
+    ~UnaryExpression() { delete term; }
+    virtual void execute();
+private:
+    Token op;
+    Expression *term;
+};
+
+class Basic::MultiplicativeExpression : public Expression {
+public:
+    MultiplicativeExpression() { }
+    ~MultiplicativeExpression();
+    virtual void execute();
+    void appendTerm(Expression *term) { terms.push_back(term); }
+    void appendOperator(Token t) { operators.push_back(t); }
+private:
+    std::list<Expression *> terms;
+    std::list<Token> operators;
+};
+
+class Basic::AdditiveExpression : public Expression {
+public:
+    AdditiveExpression() { }
+    ~AdditiveExpression();
+    virtual void execute();
+    void appendTerm(Expression *term) { terms.push_back(term); }
+    void appendOperator(Token t) { operators.push_back(t); }
+private:
+    std::list<Expression *> terms;
+    std::list<Token> operators;
+};
+
+class Basic::ComparitiveExpression : public Expression {
+public:
+    ComparitiveExpression(Expression *first, Token cmp, Expression *second) : first(first), cmp(cmp), second(second) { }
+    ~ComparitiveExpression() { delete first; delete second; }
+    virtual void execute();
+private:
+    Expression *first;
+    Token cmp;
+    Expression *second;
+};
+
+class Basic::AndExpression : public Expression {
+public:
+    AndExpression() { }
+    ~AndExpression();
+    virtual void execute();
+    void appendTerm(Expression *term) { terms.push_back(term); }
+private:
+    std::list<Expression *> terms;
+};
+
+class Basic::OrExpression : public Expression {
+public:
+    OrExpression() { }
+    ~OrExpression();
+    virtual void execute();
+    void appendTerm(Expression *term) { terms.push_back(term); }
+private:
+    std::list<Expression *> terms;
+};
+
+
+class Basic::Statement : public ASTNode { };
+
+class Basic::PrintStatement : public Statement {
+public:
+    PrintStatement() { }
+    ~PrintStatement();
+    virtual void execute();
+    void appendExpression(Expression *e) { expressions.push_back(e); }
+    void setAppendEol(bool b) { append_eol = b; }
+    
+protected:
+    std::list<Expression *> expressions;
+    bool append_eol;
+};
+
 
 class Basic::Block : public ASTNode {
 public:
-    Block();
+    Block() { }
     ~Block();
-    void appendStatement(const Statement &);
+    virtual void execute();
+    void appendStatement(Statement *s) { statements.push_back(s); }
 protected:
-    std::list<ASTNode> statements;
+    std::list<Statement *> statements;
 };
 
-class Basic::PrintStatement : public ASTNode {
-public:
-    PrintStatement();
-protected:
-    std::list<SemicolonExpression> semicolonExpressions;
-};
 
-class Basic::SemicolonExpression : public ASTNode {
-public:
-    SemicolonExpression();
-protected:
-    std::list<CommaExpression> commaExpressions;
-};
 
-class Basic::CommaExpression : public ASTNode {
-public:
-    CommaExpression();
-protected:
-    std::list<Expression> expressions;
-};
+
 
 class Basic::ForStatement : public ASTNode {
 public:
     ForStatement();
+    virtual void execute();
 protected:
     Identifier *identifier;
     Variant initialValue;
@@ -108,6 +233,7 @@ enum DoConditionWhen {
 class Basic::DoStatement : public ASTNode {
 public:
     DoStatement();
+    virtual void execute();
 protected:
     DoConditionType conditionType;
     DoConditionWhen conditionWhen;
