b9ec4b1a9cc1d52d0ed2ecc41fda4995a7bdb8f3
flrl <flrl@users.noreply.github.com>
2010-09-12 04:00:41 +0000

    Converted ASTNode classes to use m_ prefix for member variables

--- ast.cpp
+++ ast.cpp
@@ -21,12 +21,12 @@ void Basic::IdentifierExpression::execute (void) {
 }
 
 void Basic::UnaryExpression::execute (void) {
-    this->term->execute();
-    Variant intermediate = term->getResult();
-    if (this->op == TkNOT) {
+    m_term->execute();
+    Variant intermediate = m_term->getResult();
+    if (m_op == TkNOT) {
         m_result.setBoolValue(!intermediate.getBoolValue());
     }
-    else if (this->op == TkMINUS) {
+    else if (m_op == TkMINUS) {
         switch(intermediate.getType()) {
             case Basic::Variant::VaINT:
                 m_result.setIntValue(0 - intermediate.getIntValue());
@@ -40,15 +40,15 @@ void Basic::UnaryExpression::execute (void) {
         }
     }
     else {
-        fprintf(stderr, "debug: invalid operator `%s' in UnaryExpression object\n", Tokeniser::tokenDescriptions[this->op]);
+        fprintf(stderr, "debug: invalid operator `%s' in UnaryExpression object\n", Tokeniser::tokenDescriptions[m_op]);
     }
 }
 
 void Basic::MultiplicativeExpression::execute (void) {
-    first_term->execute();
-    Variant intermediate = first_term->getResult();
+    m_first_term->execute();
+    Variant intermediate = m_first_term->getResult();
     
-    for (std::list<OperatorTerm>::const_iterator term = other_terms.begin(); term != other_terms.end(); term++) {
+    for (std::list<OperatorTerm>::const_iterator term = m_other_terms.begin(); term != m_other_terms.end(); term++) {
         term->term->execute();
         Variant t = term->term->getResult();
         
@@ -64,10 +64,10 @@ void Basic::MultiplicativeExpression::execute (void) {
 }
 
 void Basic::AdditiveExpression::execute (void) {
-    first_term->execute();
-    Variant intermediate = first_term->getResult();
+    m_first_term->execute();
+    Variant intermediate = m_first_term->getResult();
     
-    for (std::list<OperatorTerm>::const_iterator term = other_terms.begin(); term != other_terms.end(); term++) {
+    for (std::list<OperatorTerm>::const_iterator term = m_other_terms.begin(); term != m_other_terms.end(); term++) {
         term->term->execute();
         Variant t = term->term->getResult();
         
@@ -84,22 +84,22 @@ void Basic::AdditiveExpression::execute (void) {
 
 void Basic::ComparitiveExpression::execute (void) {
     bool intermediate = false;
-    switch (this->cmp) {
-        case TkEQUALS:      intermediate = (first == second); break;
-        case TkNOTEQUALS:   intermediate = (first != second); break;
-        case TkLT:          intermediate = (first <  second); break;
-        case TkGT:          intermediate = (first >  second); break;
-        case TkLTEQUALS:    intermediate = (first <= second); break;
-        case TkGTEQUALS:    intermediate = (first >= second); break;
+    switch (m_cmp) {
+        case TkEQUALS:      intermediate = (m_first == m_second); break;
+        case TkNOTEQUALS:   intermediate = (m_first != m_second); break;
+        case TkLT:          intermediate = (m_first <  m_second); break;
+        case TkGT:          intermediate = (m_first >  m_second); break;
+        case TkLTEQUALS:    intermediate = (m_first <= m_second); break;
+        case TkGTEQUALS:    intermediate = (m_first >= m_second); break;
         default: 
-            fprintf(stderr, "debug: invalid operator `%s' in ComparitiveExpression object\n", Tokeniser::tokenDescriptions[this->cmp]);
+            fprintf(stderr, "debug: invalid operator `%s' in ComparitiveExpression object\n", Tokeniser::tokenDescriptions[m_cmp]);
     }
     m_result.setBoolValue(intermediate);
 }
 
 void Basic::AndExpression::execute (void) {
     bool intermediate = true;
-    for (std::list<Basic::Expression*>::const_iterator e = terms.begin(); e != terms.end(); e++) {
+    for (std::list<Basic::Expression*>::const_iterator e = m_terms.begin(); e != m_terms.end(); e++) {
         (*e)->execute();
         intermediate = (intermediate and (*e)->getResult().getBoolValue());
     }
@@ -108,7 +108,7 @@ void Basic::AndExpression::execute (void) {
 
 void Basic::OrExpression::execute (void) {
     bool intermediate = false;
-    for (std::list<Basic::Expression*>::const_iterator e = terms.begin(); e != terms.end(); e++) {
+    for (std::list<Basic::Expression*>::const_iterator e = m_terms.begin(); e != m_terms.end(); e++) {
         (*e)->execute();
         intermediate = (intermediate and (*e)->getResult().getBoolValue());
     }
@@ -116,17 +116,17 @@ void Basic::OrExpression::execute (void) {
 }
 
 void Basic::Block::execute (void) {
-    for (std::list<Basic::Statement*>::const_iterator s = statements.begin(); s != statements.end(); s++) {
+    for (std::list<Basic::Statement*>::const_iterator s = m_statements.begin(); s != m_statements.end(); s++) {
         (*s)->execute();
     }
 }
 
 void Basic::PrintStatement::execute (void) {
-    for (std::list<Basic::Expression*>::const_iterator e = expressions.begin(); e != expressions.end(); e++) {
+    for (std::list<Basic::Expression*>::const_iterator e = m_expressions.begin(); e != m_expressions.end(); e++) {
         (*e)->execute();
         fputs((*e)->getResult().getStringValue(), stdout);
     }
-    if (append_eol)  putchar('\n');
+    if (m_append_eol)  putchar('\n');
 }
 
 void Basic::InputStatement::execute (void) {
@@ -183,85 +183,85 @@ void Basic::SubDefinition::execute (void) {
 
 
 Basic::PrintStatement::~PrintStatement() {
-    for (std::list<Basic::Expression *>::iterator e = expressions.begin(); e != expressions.end(); e++) {
+    for (std::list<Basic::Expression *>::iterator e = m_expressions.begin(); e != m_expressions.end(); e++) {
         delete (*e);
     }
 }
 
 Basic::IfStatement::~IfStatement() {
-    for (std::list<ConditionalBlock>::iterator b = conditional_blocks.begin(); b != conditional_blocks.end(); b++) {
+    for (std::list<ConditionalBlock>::iterator b = m_conditional_blocks.begin(); b != m_conditional_blocks.end(); b++) {
         delete b->condition;
         delete b->block;
     }
     
-    if (else_block)  delete else_block;
+    if (m_else_block)  delete m_else_block;
 }
 
 Basic::ForStatement::~ForStatement() {
-    delete start;
-    delete end;
-    if (step)  delete step;
-    delete body;
+    delete m_start;
+    delete m_end;
+    if (m_step)  delete m_step;
+    delete m_body;
 }
 
 Basic::DimStatement::~DimStatement() {
-    for (std::list<Dimensionable>::iterator d = dimensionables.begin(); d != dimensionables.end(); d++) {
+    for (std::list<Dimensionable>::iterator d = m_dimensionables.begin(); d != m_dimensionables.end(); d++) {
         if (d->second)  delete d->second;
     }
 }
 
 Basic::ArraySubscript::~ArraySubscript() {
-    for (std::list<Basic::Expression *>::iterator e = expressions.begin(); e != expressions.end(); e++) {
+    for (std::list<Basic::Expression *>::iterator e = m_expressions.begin(); e != m_expressions.end(); e++) {
         delete (*e);
     }
 }
 
 Basic::ArrayDimension::~ArrayDimension() {
-    for (std::list<ArrayDimension::Specification>::iterator d = dimensions.begin(); d != dimensions.end(); d++) {
+    for (std::list<ArrayDimension::Specification>::iterator d = m_dimensions.begin(); d != m_dimensions.end(); d++) {
         delete d->first;
         if (d->second)  delete d->second;
     }
 }
 
 Basic::AcceptedParamList::~AcceptedParamList() {
-    
+    // FIXME
 }
 
 Basic::ParamList::~ParamList() {
-    for (std::vector<Expression *>::iterator e = expressions.begin(); e != expressions.end(); e++) {
+    for (std::vector<Expression *>::iterator e = m_expressions.begin(); e != m_expressions.end(); e++) {
         delete (*e);
     }
 }
 
 Basic::Block::~Block() {
-    for (std::list<Basic::Statement *>::iterator s = statements.begin(); s != statements.end(); s++) {
+    for (std::list<Basic::Statement *>::iterator s = m_statements.begin(); s != m_statements.end(); s++) {
         delete (*s);
     }
 }
 
 Basic::MultiplicativeExpression::~MultiplicativeExpression() {
-    delete first_term;
-    for (std::list<OperatorTerm>::iterator e = other_terms.begin(); e != other_terms.end(); e++) {
+    delete m_first_term;
+    for (std::list<OperatorTerm>::iterator e = m_other_terms.begin(); e != m_other_terms.end(); e++) {
         delete (e->term);
     }
 }
 
 Basic::AdditiveExpression::~AdditiveExpression() {
-    delete first_term;
-    for (std::list<OperatorTerm>::iterator e = other_terms.begin(); e != other_terms.end(); e++) {
+    delete m_first_term;
+    for (std::list<OperatorTerm>::iterator e = m_other_terms.begin(); e != m_other_terms.end(); e++) {
         delete (e->term);
     }
 }
 
 
 Basic::AndExpression::~AndExpression() {
-    for (std::list<Basic::Expression *>::iterator e = terms.begin(); e != terms.end(); e++) {
+    for (std::list<Basic::Expression *>::iterator e = m_terms.begin(); e != m_terms.end(); e++) {
         delete (*e);
     }
 }
 
 Basic::OrExpression::~OrExpression() {
-    for (std::list<Basic::Expression *>::iterator e = terms.begin(); e != terms.end(); e++) {
+    for (std::list<Basic::Expression *>::iterator e = m_terms.begin(); e != m_terms.end(); e++) {
         delete (*e);
     }    
 }
